7959d7b0216030743b55e7100a11ddc16693af2b
Abdirino <abdirinoahash@gmail.com>
2025-05-15 14:36:12 +0300

    uploads sec initiate

--- Upload/upload.php
+++ Upload/upload.php
@@ -1,17 +1,24 @@
-<?
-echo "helo world"
+<?php
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload</title>
     <link rel="stylesheet" href="upload.css">
 </head>
+
 <body>
-    <h1>Upload Files</h1>
-    <button>Upload</button>
+    <div class="container">
+        <h1>Upload Files</h1>
+        <button>Upload</button>
+    </div>
+
+    <script src="upload.js"></script>
 </body>
+
 </html>
